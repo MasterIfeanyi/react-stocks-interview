5db5160440d41cf47396d6b039387d368ca88c34
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-05 16:49:51 -0700

    add Header to Layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {Outlet} from "react-router-dom"
 import Footer from './Footer'
 import Navigation from './Navigation'
+import Header from './Header/Header'
 
 const Layout = () => {
   return (
     <main className='App'>
-      <Navigation />
+      <Header />
       <Outlet />
       <Footer />
     </main>
