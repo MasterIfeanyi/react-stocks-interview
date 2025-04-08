4ce65ea89e7d3df602427957b59378fd901aaf2d
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-04-08 16:09:19 -0700

    add accordion-item

--- src/components/SendMoney/FAQ.jsx
+++ src/components/SendMoney/FAQ.jsx
@@ -34,11 +34,11 @@ const FAQ = () => {
             <div class="accordion-item">
                 <h2 class="accordion-header" id="flush-headingThree">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                    What is a block chain
+                    Is Mustard a bank ?
                 </button>
                 </h2>
                 <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">Blockchain is a sophisticated database system designed to securely record transactions across many computers, these transactions are grouped into blocks, these blocks are chained together in a chronological order to create a tamper-proof record.</div>
+                <div class="accordion-body">Mustard is a financial technology company and is not a bank. RightCard Payment Services Limited is registered with the Financial Conduct Authority, in the UK as an Electronic Money Institution. In Canada, LemFi is registered and regularised by the Financial Transactions and Report Analysis Centre of Canada. In the United States, the remittance and bank services program is sponsored by Community Federal Savings Bank</div>
                 </div>
             </div>
         </div>
